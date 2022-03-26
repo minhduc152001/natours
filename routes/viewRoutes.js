d51b7dd4aca64aeba11f5b185e3a8c6cdc0423d6
Your Name <you@example.com>
2022-03-26 16:56:27 +0700

    Fix booking

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,17 +4,17 @@ const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 const viewController = require('../controllers/viewController');
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
-router.get('/signup', viewController.getSignUpForm)
+router.get('/signup', viewController.getSignUpForm);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
-router.get('/my-tours', authController.protect, viewController.getMyTours);
+router.get(
+  '/my-tours',
+  bookingController.createBookingCheckout,
+  authController.protect,
+  viewController.getMyTours
+);
 
 router.post(
   '/submit-user-data',
